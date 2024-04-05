b70cf1acaa53f1fa783af113d6822b7cdd98a1bb
cc0803 <simongeldbach@gmail.com>
2024-04-05 18:50:37 +0200

    fix: make both capitalize tests pass

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-export default function capitalize() {
-	return "My";
+export default function capitalize(word) {
+	return word.charAt(0).toUpperCase() + word.slice(1);
 }
--- index.test.js
+++ index.test.js
@@ -1,5 +1,9 @@
 import capitalize from "./index.js";
 
-test("Capitalize first letter in string", () => {
+test("Capitalize first letter in string 1", () => {
 	expect(capitalize("my")).toBe("My");
+});
+
+test("Capitalize first letter in string 2", () => {
+	expect(capitalize("dog")).toBe("Dog");
 });
